aee1debc92d29f889a73914f0840f6491cdae886
EdwardJhosep <1454668@senati.pe>
2023-08-17 19:12:33 -0500

    importacion de Scanner

---
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+//importamos Scanner
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+
+    }
+}
